22374522eb1b440b847903f5076633f0d19c9b81
Paul Diones <dionespaul@gmail.com>
2017-10-07 10:05:06 -0700

    added more artist to random search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@
 
   var database = firebase.database()
 
-  var randomArtistArray = ["Coldplay", "Muse", "Radiohead", "Queens of the Stone Age"]
+  var randomArtistArray = ["Coldplay", "Muse", "Radiohead", "Queens of the Stone Age", "Incubus", "Dr. Dre", "Eminem", "Fall Out Boy", "Jay Z", "Beyonce", "Metallica"]
 
     var randomArtist =""
   $("#random-button").on("click", function() {
